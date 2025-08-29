6e73b9dd0d3ecf1eb288d5894f98235b9bf5cab5
Pradyunn Kale <kalepradyunn@gmail.com>
2025-08-28 21:07:10 -0400

    Fixed the prototype and removed unnecessary overrides

--- drivers/adc/adc.cpp
+++ drivers/adc/adc.cpp
@@ -24,18 +24,18 @@ public:
         return ADS_OK; 
     }
 
-    error_codes_t ads_build_command(uint8_t C, uint8_t MUL, ADC_MCU_Register A) override {
+    error_codes_t ads_build_command(uint8_t C, uint8_t MUL, ADC_MCU_Register A) {
         return (C << 5) | (MUL << 4) | (A & 0x0F);
     } 
 
-    error_codes_t ads_read_register(const ADC_MCU_Register reg, uint8_t* value) override {
+    error_codes_t ads_read_register(const ADC_MCU_Register reg, uint8_t* value) {
         uint8_t cmd = build_command(CMD_RREG, 0, reg);
         if (SPI_WRITE(&cmd, 1) != 0) return ADS_WSPI_ERROR;
         if (SPI_READ(value, 1) != 0) return ADS_RSPI_ERROR;
         return ADS_OK;
     }
 
-    error_codes_t ads_write_register(ADC_MCU_Register reg, uint8_t value) override {
+    error_codes_t ads_write_register(ADC_MCU_Register reg, uint8_t value) {
         uint8_t cmd = build_command(CMD_WREG, 0, reg);
         uint8_t count = 0x00;
         if (SPI_WRITE(&cmd, 1) != 0) return ADS_WSPI_ERROR;
@@ -44,14 +44,14 @@ public:
         return ADS_OK;
     }
 
-    error_codes_t ads_read_register_mul(const ADC_MCU_Register start_reg, uint8_t* buf, uint8_t size) override {
+    error_codes_t ads_read_register_mul(const ADC_MCU_Register start_reg, uint8_t* buf, uint8_t size) {
         uint8_t cmd = build_command(CMD_RREG, 1, start_reg);
         if(SPI_WRITE(&cmd, 1) != 0) return ADS_WSPI_ERROR;
         if(SPI_READ(buf, size) != 0) return ADS_RSPI_ERROR;
         return ADS_OK;
     }
 
-    error_codes_t ads_write_register_mul(ADC_MCU_Register start_reg, uint8_t* values, uint8_t size) override {
+    error_codes_t ads_write_register_mul(ADC_MCU_Register start_reg, uint8_t* values, uint8_t size) {
         if (size == 0) return ADS_ERROR;
         uint8_t cmd = build_command(CMD_WREG, 1, start_reg);
         uint8_t count = size-1;
