8d065f9b4c3b2e070849a0856caac99ec9c082ad
Pradyunn Kale <kalepradyunn@gmail.com>
2025-08-30 11:50:47 -0400

    Included the HAL functions for the macros, also moved the structs etc within the dedicated class based on what is going on with them

--- drivers/adc/adc.h
+++ drivers/adc/adc.h
@@ -3,53 +3,19 @@
 #include <stddef.h>
 #include "stm32h7xx_hal.h"
 
-#define SPI_READ (dst, size) (0)
-#define SPI_WRITE (src, size) (0)
-
-typedef enum {
-    CMD_DATA_READ = 0x01,
-    CMD_RREG = 0x02,
-    CMD_WREG = 0x03,
-    CMD_RST = 0x06,     
-} ADC_MCU_Commands;
-
-typedef enum {
-    ADS_OK,
-    ADS_ERR,
-    ADS_RSPI_ERR,
-    ADS_WSPI_ERR,
-} ADC_ErrorCodes;
-
-typedef enum {
-    CONFIG0 = 0x00,
-    CONFIG1 = 0x01,
-    MUXSCH 	= 0x02,
-    MUXDIF 	= 0x03,
-    MUXSG0 	= 0x04,
-    MUXSG1 	= 0x05,
-    SYSRED 	= 0x06,
-    GPIOC 	= 0x07,
-    GPIOD 	= 0x08,
-    ID 		= 0x09,
-} ADC_MCU_Register;
-
-typedef enum {
-	ADS_CH_DIFF0 = 0x00,
-	ADS_CH_DIFF1 = 0x01,
-	ADS_CH_DIFF2 = 0x02,
-	ADS_CH_DIFF3 = 0x03,
-	ADS_CH_DIFF4 = 0x04,
-	ADS_CH_DIFF5 = 0x05,
-	ADS_CH_DIFF6 = 0x06,
-	ADS_CH_DIFF7 = 0x07,
-} ADC_MCU_ChannelID;
-
-typedef enum {
-    
-} ADC_SIM_ChannelID;
+#define SPI_READ (dst, size) HAL_SPI_Receive(&hspi1, (uint8_t *)src, size, HAL_MAX_DELAY)
+#define SPI_WRITE (src, size) HAL_SPI_Transmit(&hspi1, (uint8_t *)src, size, HAL_MAX_DELAY)
 
 class ADC_Driver {
 public:
+
+    typedef enum {
+        ADS_OK,
+        ADS_ERR,
+        ADS_RSPI_ERR,
+        ADS_WSPI_ERR,
+    } ADC_ErrorCodes;
+
     virtual ~ADC_Driver() = default;
     virtual ADC_ErrorCodes ads_init(void) = 0;
     virtual ADC_ErrorCodes ads_set_channel(ADC_SIM_ChannelID channel) = 0;
@@ -58,12 +24,50 @@ public:
 
 class ADC_SIM_Driver : public ADC_Driver {
 public:
+    
+    typedef enum {
+    
+    } ADC_SIM_ChannelID;
+
     ADC_ErrorCodes ads_init(void) override;
     ADC_ErrorCodes ads_set_channel(ADC_MCU_ChannelID channel) override;
     ADC_ErrorCodes ads_data_read(int32_t *result, uint8_t status) override;
 };
 
 class ADC_MCU_Driver : public ADC_Driver {
+public:
+
+    typedef enum {
+        CMD_DATA_READ = 0x01,
+        CMD_RREG = 0x02,
+        CMD_WREG = 0x03,
+        CMD_RST = 0x06,     
+    } ADC_MCU_Commands;
+
+    typedef enum {
+        CONFIG0 = 0x00,
+        CONFIG1 = 0x01,
+        MUXSCH 	= 0x02,
+        MUXDIF 	= 0x03,
+        MUXSG0 	= 0x04,
+        MUXSG1 	= 0x05,
+        SYSRED 	= 0x06,
+        GPIOC 	= 0x07,
+        GPIOD 	= 0x08,
+        ID 		= 0x09,
+    } ADC_MCU_Register;
+
+    typedef enum {
+        ADS_CH_DIFF0 = 0x00,
+        ADS_CH_DIFF1 = 0x01,
+        ADS_CH_DIFF2 = 0x02,
+        ADS_CH_DIFF3 = 0x03,
+        ADS_CH_DIFF4 = 0x04,
+        ADS_CH_DIFF5 = 0x05,
+        ADS_CH_DIFF6 = 0x06,
+        ADS_CH_DIFF7 = 0x07,
+    } ADC_MCU_ChannelID;
+
     ADC_ErrorCodes ads_init(void) override;
     ADC_ErrorCodes ads_set_channel(ADC_MCU_ChannelID channel) override;
     ADC_ErrorCodes ads_data_read(int32_t *result, uint8_t status) override;
