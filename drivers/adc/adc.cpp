cfaaff5c0a22971f236627752419aabcd5f4a600
Pradyunn Kale <kalepradyunn@gmail.com>
2025-08-31 16:40:15 -0400

    Got everything to build by fixing syntax heavily, also uncommented the SPI module in the HAL config

--- drivers/adc/adc.cpp
+++ drivers/adc/adc.cpp
@@ -1,79 +1,77 @@
 #include "adc.h"
 
-class ADC_SIM_Driver : public ADC_Driver {
-public:
-    ADC_ErrorCodes ads_init(void) override {
-        
-    }
 
-    ADC_ErrorCodes ads_set_channel(ADC_SIM_ChannelID channel) override {
 
-    }
+ADC_Driver::ErrorCodes ADC_SIM_Driver::init(void) {
+    return ErrorCodes::ADS_OK; 
+}
 
-    ADC_ErrorCodes ads_data_read(int32_t *result, uint8_t status) override {
+ADC_Driver::ErrorCodes ADC_SIM_Driver::set_channel(ADC_Driver::ChannelID channel) {
+    return ErrorCodes::ADS_OK;
+}
 
-    }
+ADC_Driver::ErrorCodes ADC_SIM_Driver::data_read(int32_t *result, uint8_t *status) {
+    return ErrorCodes::ADS_OK;
+}
 
-};
 
-class ADC_MCU_Driver : public ADC_Driver {
-public:
-    ADC_ErrorCodes ads_init(void) override {
-        uint8_t cmd = ads_build_command(CMD_RST, 0, 0,);
-        if(SPI_WRITE(&cmd, 1) != 0) return ADS_WSPI_ERROR;
-        return ADS_OK; 
-    }
 
-    ADC_ErrorCodes ads_build_command(uint8_t C, uint8_t MUL, ADC_MCU_Register A) {
-        return (C << 5) | (MUL << 4) | (A & 0x0F);
-    } 
+ADC_Driver::ErrorCodes ADC_MCU_Driver::init(void) {
+    uint8_t cmd = build_command(Commands::CMD_RST, 0, Register::CONFIG0);
+    if(SPI_WRITE(&cmd, 1) != 0) return ErrorCodes::ADS_WSPI_ERR;
+    return ErrorCodes::ADS_OK; 
+}
 
-    ADC_ErrorCodes ads_read_register(const ADC_MCU_Register reg, uint8_t* value) {
-        uint8_t cmd = build_command(CMD_RREG, 0, reg);
-        if (SPI_WRITE(&cmd, 1) != 0) return ADS_WSPI_ERROR;
-        if (SPI_READ(value, 1) != 0) return ADS_RSPI_ERROR;
-        return ADS_OK;
-    }
+uint8_t ADC_MCU_Driver::build_command(Commands C, uint8_t MUL, Register A) {
+    return (static_cast<uint8_t>(C) << 5) | (MUL << 4) | (static_cast<uint8_t>(A) & 0x0F);
+} 
 
-    ADC_ErrorCodes ads_write_register(ADC_MCU_Register reg, uint8_t value) {
-        uint8_t cmd = build_command(CMD_WREG, 0, reg);
-        uint8_t count = 0x00;
-        if (SPI_WRITE(&cmd, 1) != 0) return ADS_WSPI_ERROR;
-        if (SPI_WRITE(&count, 1) != 0) return ADS_WSPI_ERROR;
-        if (SPI_WRITE(&value, 1) != 0) return ADS_WSPI_ERROR;
-        return ADS_OK;
-    }
+ADC_Driver::ErrorCodes ADC_MCU_Driver::read_register(const Register reg, uint8_t* value) {
+    uint8_t cmd = build_command(Commands::CMD_RREG, 0, reg);
+    if (SPI_WRITE(&cmd, 1) != 0) return ErrorCodes::ADS_WSPI_ERR;
+    if (SPI_READ(value, 1) != 0) return ErrorCodes::ADS_RSPI_ERR;
+    return ErrorCodes::ADS_OK;
+}
 
-    ADC_ErrorCodes ads_read_register_mul(const ADC_MCU_Register start_reg, uint8_t* buf, uint8_t size) {
-        uint8_t cmd = build_command(CMD_RREG, 1, start_reg);
-        if(SPI_WRITE(&cmd, 1) != 0) return ADS_WSPI_ERROR;
-        if(SPI_READ(buf, size) != 0) return ADS_RSPI_ERROR;
-        return ADS_OK;
-    }
+ADC_Driver::ErrorCodes ADC_MCU_Driver::write_register(Register reg, uint8_t value) {
+    uint8_t cmd = build_command(Commands::CMD_WREG, 0, reg);
+    uint8_t count = 0x00;
+    if (SPI_WRITE(&cmd, 1) != 0) return ErrorCodes::ADS_WSPI_ERR;
+    if (SPI_WRITE(&count, 1) != 0) return ErrorCodes::ADS_WSPI_ERR;
+    if (SPI_WRITE(&value, 1) != 0) return ErrorCodes::ADS_WSPI_ERR;
+    return ErrorCodes::ADS_OK;
+}
 
-    ADC_ErrorCodes ads_write_register_mul(ADC_MCU_Register start_reg, uint8_t* values, uint8_t size) {
-        if (size == 0) return ADS_ERROR;
-        uint8_t cmd = build_command(CMD_WREG, 1, start_reg);
-        uint8_t count = size-1;
-        if (SPI_WRITE(&cmd, 1) != 0) return ADS_WSPI_ERROR;
-        if (SPI_WRITE(&count, 1) != 0) return ADS_WSPI_ERROR;
-        if (SPI_WRITE(values, size) != 0) return ADS_WSPI_ERROR;
-        return ADS_OK;
-    }
+ADC_Driver::ErrorCodes ADC_MCU_Driver::read_register_mul(const Register start_reg, uint8_t* buf, uint8_t size) {
+    uint8_t cmd = build_command(Commands::CMD_RREG, 1, start_reg);
+    if(SPI_WRITE(&cmd, 1) != 0) return ErrorCodes::ADS_WSPI_ERR;
+    if(SPI_READ(buf, size) != 0) return ErrorCodes::ADS_RSPI_ERR;
+    return ErrorCodes::ADS_OK;
+}
 
-    ADC_ErrorCodes ads_set_channel(ADC_MCU_ChannelID channel) override {
-        return ads_write_register(MUXDIF, (uint8_t) channel);
-    }
-    
-    ADC_ErrorCodes ads_data_read(int32_t *result, uint8_t status) override {
-	    if (result == NULL || status == NULL) return ADS_ERROR;
-        uint8_t cmd = build_command(CMD_DATA_READ, 1, 0);
-        uint8_t rx_buf[4] = {0};
-        if (SPI_WRITE(&cmd, 1) != 0) return ADS_WSPI_ERROR;
-        if(SPI_READ(rx_buf, 4) != 0) return ADS_RSPI_ERROR;
-        int32_t raw24 = (rx_buf[1] << 16) | (rx_buf[2] << 8) | (rx_buf[3]);
-        *result = (int32_t)(raw24 << 8) >> 8;
-        *status = rx_buf[0];
-        return ADS_OK;
-    }
-};
+ADC_Driver::ErrorCodes ADC_MCU_Driver::write_register_mul(Register start_reg, uint8_t* values, uint8_t size) {
+    if (size == 0) return ErrorCodes::ADS_ERR;
+    uint8_t cmd = build_command(Commands::CMD_WREG, 1, start_reg);
+    uint8_t count = size-1;
+    if (SPI_WRITE(&cmd, 1) != 0) return ErrorCodes::ADS_WSPI_ERR;
+    if (SPI_WRITE(&count, 1) != 0) return ErrorCodes::ADS_WSPI_ERR;
+    if (SPI_WRITE(values, size) != 0) return ErrorCodes::ADS_WSPI_ERR;
+    return ErrorCodes::ADS_OK;
+}
+
+ADC_Driver::ErrorCodes ADC_MCU_Driver::set_channel(ChannelID channel) {
+    return write_register(Register::MUXDIF, (uint8_t) channel);
+}
+
+ADC_Driver::ErrorCodes ADC_MCU_Driver::data_read(int32_t *result, uint8_t *status) {
+    if (result == NULL || status == NULL) return ErrorCodes::ADS_ERR;
+    uint8_t cmd = build_command(Commands::CMD_DATA_READ, 1, Register::CONFIG0);
+    uint8_t rx_buf[4] = {0};
+    if (SPI_WRITE(&cmd, 1) != 0) return ErrorCodes::ADS_WSPI_ERR;
+    if(SPI_READ(rx_buf, 4) != 0) return ErrorCodes::ADS_RSPI_ERR;
+    int32_t raw24 = (rx_buf[1] << 16) | (rx_buf[2] << 8) | (rx_buf[3]);
+    *result = (int32_t)(raw24 << 8) >> 8;
+    *status = rx_buf[0];
+    return ErrorCodes::ADS_OK;
+}
+
--- drivers/adc/adc.h
+++ drivers/adc/adc.h
@@ -3,61 +3,22 @@
 #include <stddef.h>
 #include "stm32h7xx_hal.h"
 
-#define SPI_READ (dst, size) HAL_SPI_Receive(&hspi1, (uint8_t *)src, size, HAL_MAX_DELAY)
-#define SPI_WRITE (src, size) HAL_SPI_Transmit(&hspi1, (uint8_t *)src, size, HAL_MAX_DELAY)
+extern SPI_HandleTypeDef hspi1;
+
+#define SPI_READ(dst, size) HAL_SPI_Receive(&hspi1, (uint8_t *)dst , size, HAL_MAX_DELAY)
+#define SPI_WRITE(src, size) HAL_SPI_Transmit(&hspi1, (uint8_t *)src, size, HAL_MAX_DELAY)
 
 class ADC_Driver {
 public:
 
-    enum class {
+    enum class ErrorCodes {
         ADS_OK,
         ADS_ERR,
         ADS_RSPI_ERR,
         ADS_WSPI_ERR,
-    } ADC_ErrorCodes;
+    };
 
-    virtual ~ADC_Driver() = default;
-    virtual ADC_ErrorCodes ads_init(void) = 0;
-    virtual ADC_ErrorCodes ads_set_channel(ADC_SIM_ChannelID channel) = 0;
-    virtual ADC_ErrorCodes ads_data_read(int32_t *result, uint8_t status) = 0;
-};
-
-class ADC_SIM_Driver : public ADC_Driver {
-public:
-    
-    enum class {
-    
-    } ADC_SIM_ChannelID;
-
-    ADC_ErrorCodes ads_init(void) override;
-    ADC_ErrorCodes ads_set_channel(ADC_MCU_ChannelID channel) override;
-    ADC_ErrorCodes ads_data_read(int32_t *result, uint8_t status) override;
-};
-
-class ADC_MCU_Driver : public ADC_Driver {
-public:
-
-    enum class {
-        CMD_DATA_READ = 0x01,
-        CMD_RREG = 0x02,
-        CMD_WREG = 0x03,
-        CMD_RST = 0x06,     
-    } ADC_MCU_Commands;
-
-    enum class {
-        CONFIG0 = 0x00,
-        CONFIG1 = 0x01,
-        MUXSCH 	= 0x02,
-        MUXDIF 	= 0x03,
-        MUXSG0 	= 0x04,
-        MUXSG1 	= 0x05,
-        SYSRED 	= 0x06,
-        GPIOC 	= 0x07,
-        GPIOD 	= 0x08,
-        ID 		= 0x09,
-    } ADC_MCU_Register;
-
-    enum class {
+    enum class ChannelID {
         ADS_CH_DIFF0 = 0x00,
         ADS_CH_DIFF1 = 0x01,
         ADS_CH_DIFF2 = 0x02,
@@ -66,13 +27,52 @@ public:
         ADS_CH_DIFF5 = 0x05,
         ADS_CH_DIFF6 = 0x06,
         ADS_CH_DIFF7 = 0x07,
-    } ADC_MCU_ChannelID;
+    };
 
-    ADC_ErrorCodes ads_init(void) override;
-    ADC_ErrorCodes ads_set_channel(ADC_MCU_ChannelID channel) override;
-    ADC_ErrorCodes ads_data_read(int32_t *result, uint8_t status) override;
-    ADC_ErrorCodes ads_build_command(uint8_t C, uint8_t MUL, ADC_MCU_Register A);
-    ADC_ErrorCodes ads_write_register(ADC_MCU_Register reg, uint8_t value);
-    ADC_ErrorCodes ads_read_register_mul(const ADC_MCU_Register start_reg, uint8_t* buf, uint8_t size);
-    ADC_ErrorCodes ads_write_register_mul(ADC_MCU_Register start_reg, uint8_t* values, uint8_t size); 
+    virtual ~ADC_Driver() = default;
+    virtual ErrorCodes init(void) = 0;
+    virtual ErrorCodes set_channel(ChannelID channel) = 0;
+    virtual ErrorCodes data_read(int32_t *result, uint8_t *status) = 0;
+};
+
+class ADC_SIM_Driver : public ADC_Driver {
+public:
+
+    ADC_Driver::ErrorCodes init(void) override;
+    ADC_Driver::ErrorCodes set_channel(ADC_Driver::ChannelID channel) override;
+    ADC_Driver::ErrorCodes data_read(int32_t *result, uint8_t *status) override;
+};
+
+class ADC_MCU_Driver : public ADC_Driver {
+public:
+    
+
+    enum class Commands {
+        CMD_DATA_READ = 0x01,
+        CMD_RREG = 0x02,
+        CMD_WREG = 0x03,
+        CMD_RST = 0x06,     
+    };
+
+    enum class Register {
+        CONFIG0 = 0x00,
+        CONFIG1 = 0x01,
+        MUXSCH 	= 0x02,
+        MUXDIF 	= 0x03,
+        MUXSG0 	= 0x04,
+        MUXSG1 	= 0x05,
+        SYSRED 	= 0x06,
+        GPIO_C 	= 0x07,
+        GPIO_D 	= 0x08,
+        ID 		= 0x09,
+    };
+
+    ADC_Driver::ErrorCodes init(void) override;
+    ADC_Driver::ErrorCodes set_channel(ChannelID channel) override;
+    ADC_Driver::ErrorCodes data_read(int32_t *result, uint8_t *status) override;
+    uint8_t build_command(Commands C, uint8_t MUL, Register A);
+    ADC_Driver::ErrorCodes read_register(const Register reg, uint8_t* value);
+    ADC_Driver::ErrorCodes write_register(Register reg, uint8_t value);
+    ADC_Driver::ErrorCodes read_register_mul(const Register start_reg, uint8_t* buf, uint8_t size);
+    ADC_Driver::ErrorCodes write_register_mul(Register start_reg, uint8_t* values, uint8_t size); 
 };
