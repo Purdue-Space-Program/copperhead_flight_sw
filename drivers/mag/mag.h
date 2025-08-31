b3e7382a551f2549f003779b99f164ebaf745246
Pradyunn Kale <kalepradyunn@gmail.com>
2025-08-31 17:30:03 -0400

    Fixed syntax for Magnetometer driver and it builds now

--- drivers/mag/mag.h
+++ drivers/mag/mag.h
@@ -3,45 +3,47 @@
 #include <stddef.h>
 #include "stm32h7xx_hal.h"
 
+extern I2C_HandleTypeDef hi2c1;
+
 #define I2C_READ(device_addr, reg_addr, buffer, len) \
 HAL_I2C_Mem_Read(&hi2c1, (device_addr) << 1, (reg_addr), I2C_MEMADD_SIZE_8BIT, (uint8_t *)(buffer), (len), HAL_MAX_DELAY)
 #define I2C_WRITE(device_addr, reg_addr, data, len) \
 HAL_I2C_Mem_Write(&hi2c1, (device_addr) << 1, (reg_addr), I2C_MEMADD_SIZE_8BIT, (uint8_t *)(data), (len), HAL_MAX_DELAY)
 #define BMM_350_I2C_ADDRESS 0x10
 
-class MAG_Driver {
+class Mag_Driver {
 public:
         
-    struct {
+    struct Magnetic_Field_Vector {
         int32_t x;
         int32_t y;
         int32_t z;
-    } Magnetic_Field_Vector;
+    };
 
-    enum class {
+    enum class ErrorCodes {
         BMM_OK = 0,
         BMM_ERR_INIT = -1,
         BMM_ERR_READ_REG = -2,
         BMM_ERR_GET_STATUS = -3,
         BMM_ERR_GET_DATA = -4,
         BMM_ERR_COMBINE_AXIS_DATA = -5,
-    } MAG_ErrorCodes;
+    };
 
-    virtual ~MAG_Driver() = default;
-    virtual MAG_ErrorCodes bmm_init(void) = 0;
-    virtual MAG_ErrorCodes bmm_get_data(Magnetic_Field_Vector *data) = 0;
+    virtual ~Mag_Driver() = default;
+    virtual ErrorCodes bmm_init(void) = 0;
+    virtual ErrorCodes bmm_get_data(Magnetic_Field_Vector *data) = 0;
 };
 
-class MAG_SIM_Driver : public MAG_Driver {
+class Mag_SIM_Driver : public Mag_Driver {
 public:
-    MAG_ErrorCodes bmm_init(void) override;
-    MAG_ErrorCodes bmm_get_data(Magnetic_Field_Vector *data) override;
+    Mag_Driver::ErrorCodes bmm_init(void) override;
+    Mag_Driver::ErrorCodes bmm_get_data(Mag_Driver::Magnetic_Field_Vector *data) override;
 };
 
-class Mag_MCU_Driver : public MAG_Driver {
+class Mag_MCU_Driver : public Mag_Driver {
 public:
 
-    enum class {
+    enum class Registers {
         CMD         = 0x7E,
         INT_STATUS  = 0x30,
         MAG_X_XLSB  = 0x31,
@@ -53,10 +55,10 @@ public:
         MAG_Z_XLSB  = 0x37,
         MAG_Z_LSB   = 0x38,
         MAG_Z_MSB   = 0x39,
-    } MAG_MCU_Registers;
+    };
 
-    MAG_ErrorCodes bmm_init(void) override;
-    MAG_ErrorCodes bmm_read_register(registers_t start_reg, uint8_t *values, size_t len);
-    MAG_ErrorCodes bmm_get_status(uint8_t *status);
-    MAG_ErrorCodes bmm_get_data(Magnetic_Field_Vector *data) override;
+    Mag_Driver::ErrorCodes bmm_init(void) override;
+    Mag_Driver::ErrorCodes bmm_read_register(Registers start_reg, uint8_t *values, size_t len);
+    Mag_Driver::ErrorCodes bmm_get_status(uint8_t *status);
+    Mag_Driver::ErrorCodes bmm_get_data(Mag_Driver::Magnetic_Field_Vector *data) override;
 };
