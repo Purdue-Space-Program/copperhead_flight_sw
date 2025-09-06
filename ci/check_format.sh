9112b5fec030adfa2f23958ec55ad57926c9ec1f
Charlie Schoettle <cschoettle981@gmail.com>
2025-09-06 14:32:19 -0400

    updated formatting ci

--- ci/check_format.sh
+++ ci/check_format.sh
@@ -5,7 +5,7 @@ clang-format -i $files -Werror -n > /dev/null 2>&1
 output=$?
 
 if [[ "$output" -ne 0 ]]; then
-    echo -e "\033[1;31mStyle error detected! Make sure to run clang-format!\033[0m"
+    echo -e "\033[1;31mStyle error detected! Make sure to run clang-format via \"./tools/format.sh\"!\033[0m"
     exit 1
 else
     echo -e "\033[0;32mGood format detected!\033[0m"
