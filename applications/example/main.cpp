a55d80780f30fe210b2d67f9dc813b6e420e04fc
Pradyunn Kale <kalepradyunn@gmail.com>
2025-09-09 03:07:34 -0400

    Testing

--- applications/example/main.cpp
+++ applications/example/main.cpp
@@ -7,18 +7,25 @@
 
 #include <chrono>
 
+#include "stm32h7xx_hal.h"
+
 void PrintTask(void *argument);
+void BlinkTask(void *argument);
 
 int main(void)
 {
+    HAL_Init();
+    SystemClock_Config();
+    MX_GPIO_Init();
     StackType_t task_stack[1024] = {0};
     StaticTask_t idk_bruh[1024] = {0};
-    xTaskCreateStatic(PrintTask, "Print", 256, NULL, 1, task_stack, idk_bruh);
-
+    /*xTaskCreateStatic(PrintTask, "Print", 256, NULL, 1, task_stack, idk_bruh);*/
+    xTaskCreateStatic(BlinkTask, "Blink", 128, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while (true)
     {
+        
     }
 }
 
@@ -34,5 +41,21 @@ void PrintTask(void *argument)
 
         auto time = std::chrono::system_clock::now().time_since_epoch().count();
         printf("[%ld] Hello world!\n", time);
+    }
+}
+
+void BlinkTask(void *argument) {
+    (void)argument;
+
+    TickType_t lastWakeTime = xTaskGetTickCount();
+
+    for (;;)
+    {
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_0);
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_1);
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_2);
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_3);
+        vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(1000));
+
     }
 }
