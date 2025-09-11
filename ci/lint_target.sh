eacc456b5c71dee767dfbc5b59b8aa16c2b98fd0
Charlie Schoettle <cschoettle981@gmail.com>
2025-09-11 18:03:09 -0400

    added support for various GCC versions

--- ci/lint_target.sh
+++ ci/lint_target.sh
@@ -3,12 +3,15 @@
 rm -rf build/
 
 cmake -B build -G Ninja -DTARGET=$1
+
+GCC_VER=$(arm-none-eabi-gcc -dumpversion)
+
 run-clang-tidy \
-    -p build \
-    -extra-arg=-isystem/usr/lib/arm-none-eabi/include \
-    -extra-arg=-isystem/usr/lib/gcc/arm-none-eabi/10.3.1/include \
-    -extra-arg=-isystem/usr/lib/gcc/arm-none-eabi/10.3.1/include-fixed \
-    -extra-arg=-isystem/usr/lib/arm-none-eabi/include/c++/10.3.1 \
-    -extra-arg=-isystem/usr/lib/arm-none-eabi/include/c++/10.3.1/arm-none-eabi \
-    -extra-arg=-isystem/usr/lib/arm-none-eabi/include/c++/10.3.1/backward \
-    applications/ drivers/
+  -p build \
+  -extra-arg=-isystem/usr/lib/arm-none-eabi/include \
+  -extra-arg=-isystem/usr/lib/gcc/arm-none-eabi/$GCC_VER/include \
+  -extra-arg=-isystem/usr/lib/gcc/arm-none-eabi/$GCC_VER/include-fixed \
+  -extra-arg=-isystem/usr/lib/arm-none-eabi/include/c++/$GCC_VER \
+  -extra-arg=-isystem/usr/lib/arm-none-eabi/include/c++/$GCC_VER/arm-none-eabi \
+  -extra-arg=-isystem/usr/lib/arm-none-eabi/include/c++/$GCC_VER/backward \
+  applications/ drivers/
