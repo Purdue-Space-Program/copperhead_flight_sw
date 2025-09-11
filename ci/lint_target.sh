5d4a357302a6cc0f29918e6d7a11c24bd45fa999
Charlie Schoettle <cschoettle981@gmail.com>
2025-09-11 18:01:22 -0400

    fixed clang header issues

--- ci/lint_target.sh
+++ ci/lint_target.sh
@@ -3,4 +3,12 @@
 rm -rf build/
 
 cmake -B build -G Ninja -DTARGET=$1
-run-clang-tidy -p build applications/ drivers/
+run-clang-tidy \
+    -p build \
+    -extra-arg=-isystem/usr/lib/arm-none-eabi/include \
+    -extra-arg=-isystem/usr/lib/gcc/arm-none-eabi/10.3.1/include \
+    -extra-arg=-isystem/usr/lib/gcc/arm-none-eabi/10.3.1/include-fixed \
+    -extra-arg=-isystem/usr/lib/arm-none-eabi/include/c++/10.3.1 \
+    -extra-arg=-isystem/usr/lib/arm-none-eabi/include/c++/10.3.1/arm-none-eabi \
+    -extra-arg=-isystem/usr/lib/arm-none-eabi/include/c++/10.3.1/backward \
+    applications/ drivers/
