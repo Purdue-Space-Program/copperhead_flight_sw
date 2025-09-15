2fd6e118d980d49457a2c06a6eb95d253e80c117
Pradyunn Kale <kalepradyunn@gmail.com>
2025-09-15 00:14:51 -0400

    Keeping here to check other branch for native build

--- applications/example/main.cpp
+++ applications/example/main.cpp
@@ -7,10 +7,11 @@
 
 #include <chrono>
 
-
+#ifndef Native
 extern "C" {
 #include "stm32h7xx_hal.h"
 }
+#endif
 
 
 
@@ -20,20 +21,45 @@ void BlinkTask(void *argument);
 #define TASK_STACK_SIZE 1024
 #define TASK_STATIC_SIZE 1024
 
+#define RCC_AHB4ENR (0x58024400 + 0x0E0)
+#define GPIOB_BASE 0x58020400
+
+void simple_delay(volatile uint32_t count) {
+    while(count--) {
+        __asm__("nop");
+    }
+}
+
 int main(void)
 {
-    HAL_Init();
-    __HAL_RCC_GPIOE_CLK_ENABLE();
+    /* HAL_Init();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
 
     GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-    GPIO_InitStruct.Pin = GPIO_PIN_1;
+    GPIO_InitStruct.Pin = GPIO_PIN_7;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 
-    HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);*/
     
+    *(volatile uint32_t*)(RCC_AHB4ENR) |= (1 << 1);
+    
+    *(volatile uint32_t*)(GPIOB_BASE) |= (1 << 0);
+    *(volatile uint32_t*)(GPIOB_BASE) &= ~(1 << 1);
+
+    *(volatile uint32_t*)(GPIOB_BASE + 0x04) &= ~(1 << 0);
+
+
+    while (1) {
+
+        *(volatile uint32_t*)(GPIOB_BASE + 0x18) = (1 << 0);
+        simple_delay(2000000);
+        *(volatile uint32_t*)(GPIOB_BASE + 0x18) = (1 << 16);
+        simple_delay(2000000);
+
+    }
     /*
     StackType_t task_stack[1024] = {0};
     StaticTask_t idk_bruh[1024] = {0};
@@ -45,11 +71,6 @@ int main(void)
 
     vTaskStartScheduler();
     */
-    while (true)
-    {
-        HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_1);
-        HAL_Delay(1000);
-    }
 }
 
 /* PrintTask: prints a message every 1000 ms */
