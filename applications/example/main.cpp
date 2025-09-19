11f93985b175c099644cd980397517e577527bda
Pradyunn Kale <kalepradyunn@gmail.com>
2025-09-19 17:45:50 -0400

    Formatted

--- applications/example/main.cpp
+++ applications/example/main.cpp
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
-//Temp for testing flash
+// Temp for testing flash
 #define RCC_AHB4ENR (0x58024400 + 0x0E0)
 #define GPIOB_BASE 0x58020400
 #define DELAY 1000000
@@ -9,23 +9,27 @@
 #define BRB0 16
 #define BSBR 0x18
 
-void simple_delay(uint32_t count) {
-    while (count--) {
+void simple_delay(uint32_t count)
+{
+    while (count--)
+    {
         asm("nop");
     }
 }
 
-int main() {
-    
-    //Clock
+int main()
+{
+
+    // Clock
     *(volatile uint32_t *)(RCC_AHB4ENR) |= (1 << 1);
-    //Set as output
+    // Set as output
     *(volatile uint32_t *)(GPIOB_BASE) |= (1 << 0);
     *(volatile uint32_t *)(GPIOB_BASE) &= ~(1 << 1);
-    //Push pull output
+    // Push pull output
     *(volatile uint32_t *)(GPIOB_BASE + 0x04) &= ~(1 << 0);
 
-    while (true) {
+    while (true)
+    {
         *(volatile uint32_t *)(GPIOB_BASE + BSBR) = (1 << BSB0);
         simple_delay(DELAY);
         *(volatile uint32_t *)(GPIOB_BASE + BSBR) = (1 << BRB0);
