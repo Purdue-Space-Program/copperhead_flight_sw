d30577d9c7482e30c106795bc2c6e647ae0a9b91
Pradyunn Kale <kalepradyunn@gmail.com>
2025-09-19 11:27:09 -0400

    Updated README, and cleaned up test

--- applications/example/main.cpp
+++ applications/example/main.cpp
@@ -1,41 +1,42 @@
-#include "stm32h7xx_hal.h"
 #include <stdio.h>
-#include "FreeRTOS.h"
 #include "task.h"
+#include <stdint.h>
 
-
+//Temp for testing flash
+#define RCC_AHB4ENR (0x58024400 + 0x0E0)
+#define GPIOB_BASE 0x58020400
+#define DELAY 1000000
+#define BSB0 0
+#define BRB0 16
+#define BSBR 0x18
 #define DELAY 2000000
 
-void blink_task(void* pvParameters) {
-    
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = GPIO_PIN_0;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-    while (true) {
-        // Toggle LED
-        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
-        vTaskDelay(pdMS_TO_TICKS(DELAY));
+void simple_delay(uint32_t count) {
+    while (count--) {
+        asm("nop");
     }
 }
 
-
 int main() {
-
-    HAL_Init();
-
-    xTaskCreate(blink_task, "Blink Task", 128, NULL, 1, NULL);
-    vTaskStartScheduler();
-    while (true) {
     
+    //Clock
+    *(volatile uint32_t *)(RCC_AHB4ENR) |= (1 << 1);
+    //Set as output
+    *(volatile uint32_t *)(GPIOB_BASE) |= (1 << 0);
+    *(volatile uint32_t *)(GPIOB_BASE) &= ~(1 << 1);
+    //Push pull output
+    *(volatile uint32_t *)(GPIOB_BASE + 0x04) &= ~(1 << 0);
+
+    while (true) {
+        *(volatile uint32_t *)(GPIOB_BASE + BSBR) = (1 << BSB0);
+        simple_delay(DELAY);
+        *(volatile uint32_t *)(GPIOB_BASE + BSBR) = (1 << BRB0);
+        simple_delay(DELAY);
     }
 
     return 0;
 }
+
 
 void assert_failed(uint8_t* file, uint32_t line) {
     printf("Assertion failed in file %s on line %lu\n", file, line);
